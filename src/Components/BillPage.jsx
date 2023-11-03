be740d3d8c1eabd546d635ff961d3b80c45e99f5
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-11-03 20:40:31 +0530

    check commit

--- src/Components/BillPage.jsx
+++ src/Components/BillPage.jsx
@@ -1,7 +1,12 @@
+import LeftBox from "./LeftBox";
+import RightBox from "./RightBox";
+
 const BillPage = () => {
   return (
     <div className="billbox">
       {/* Add LeftBox and RightBox component here */}
+      <LeftBox />
+      <RightBox />
     </div>
   );
 };
